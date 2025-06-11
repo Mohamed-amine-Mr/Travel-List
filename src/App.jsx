0869b348fe31507014a9d50963020fc42ff9b4ae
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-06-11 07:01:36 +0100

    feat: lift state up to App for sharing newItem between Form and PackingList
    Lifted the state for description and quantity up to the App component, making App the common parent of Form and PackingList. This allows us to pass the newItem object from Form to PackingList via props, since sibling components cannot share state directly. This prepares the structure for managing and displaying the packing list items.
    After this step, we will pass the newItem object to PackingList and map over the items to render them in the UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import "./index.css";
 import { useState } from "react";
 function App() {
-  return (
-    <div className="app">
-      <h1>🌴 Farm Away 💼</h1>
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
-  );
-}
-
-function Form() {
   const [description,setDescritpion]=useState("");
   const [quantity,setQuantity]=useState(1);
   
@@ -24,16 +13,41 @@ function Form() {
     console.log(newItem);
   }
   return (
-    <form className="add-form" onSubmit={handleSubmit}>  
+    <div className="app">
+      <h1>🌴 Farm Away 💼</h1>
+      <Form 
+        handleSubmit={handleSubmit} 
+        setQuantity={setQuantity}
+        quantity={quantity}
+        description={description}
+        setDescritpion={setDescritpion}/>
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Form({ onSubmit, onQuantityChange, quantity, description, onDescriptionChange }) {
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select onChange={(e)=>setQuantity(Number(e.target.value))} value={quantity}>{/* we add Number() because e.target.value return string and we want number */}
+      <select
+        onChange={(e) => onQuantityChange(Number(e.target.value))}
+        value={quantity}
+      > 
+        {/* we add Number() because e.target.value return string and we want number */}
         {Array.from({ length: 20 }, (_, i) => (
           <option value={i + 1} key={i + 1}>
             {i + 1}
           </option>
         ))}
       </select>
-      <input value={description} type="text" placeholder="Item..." onChange={(e)=>setDescritpion(e.target.value)}/>
+        <input
+        value={description}
+        type="text"
+        placeholder="Item..."
+        onChange={(e) => onDescriptionChange(e.target.value)}
+      />     
       <button>Add</button>
     </form>
   );
