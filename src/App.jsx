1e8d8ab326b2de1c6001ffd97468e5bf3404f9da
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-06-11 06:41:05 +0100

    fix(form): add value prop to select to make it controlled and reset on submit
    Added the value={quantity} prop to the select element so it is controlled by React state. This ensures the select resets to 1 when the form is submitted and state is reset. Also clarified the comment about using Number() for value conversion.

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>  
       <h3>What do you need for your trip?</h3>
-      <select onChange={(e)=>setQuantity(Number(e.target.value))}>{/* we add Number() because e.target.value return string and we want number */}
+      <select onChange={(e)=>setQuantity(Number(e.target.value))} value={quantity}>{/* we add Number() because e.target.value return string and we want number */}
         {Array.from({ length: 20 }, (_, i) => (
           <option value={i + 1} key={i + 1}>
             {i + 1}
